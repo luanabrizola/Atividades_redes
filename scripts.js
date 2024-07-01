f231dd74a86c8fe1f9b7a79806688a57c642a961
gimenezhiago <130062674+luanabrizola@users.noreply.github.com>
2024-07-01 14:35:18 -0300

    Comentando código

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//capturando os elementos do formulário e html
 var form = document.getElementById("form")
 var mascara = form.elements['masc']
 var subrede = form.elements['subrede']
@@ -14,10 +15,12 @@ var container = document.querySelector("#container")
 var resultado = document.querySelector("#resultado")
 var loader = document.querySelector("#loader")
 
-btnLimpar.addEventListener("click", handleBtnLimpar)
-btnVoltar.addEventListener("click", handleBtnVoltar)
-bntCalcular.addEventListener("click", handleBtnCalcular)
+//reconhecendo o evento dos botões
+btnLimpar.addEventListener("click", handleBtnLimpar) //limpar
+btnVoltar.addEventListener("click", handleBtnVoltar) //voltar
+bntCalcular.addEventListener("click", handleBtnCalcular) //calcular
 
+//função que transforma os valores de entrada para vazio 
 function handleBtnLimpar() {
     if (subrede.value !== '' || endereco.value !== '' || mascara.value !== '') {
         subrede.value = ''
@@ -26,26 +29,29 @@ function handleBtnLimpar() {
     }
 }
 
+//função que atualiza a pagina exibindo o container principal e "esconde os resultados"
 function handleBtnVoltar() {
     container.style.display = "block"
     resultado.style.display = "none"
 }
 
+//função que verifica se todos os campos estão preenchidos e se estiver o loander e depois do setTimeout 
+//realiza os calculos e mostra os resultados
 function handleBtnCalcular(){
-    if (endereco.value === '' || mascara.value === '' || subrede.value === '') {
+    if (endereco.value === '' || mascara.value === '' || subrede.value === '') { //verificando se esta vazio
         container.style.display = "block"
         resultado.style.display = "none"
         loader.style.display = "none"
-        alert("Preencha todos os campos para calcular!")
+        alert("Preencha todos os campos para calcular!") //se sim, exibe um alerta
         return;
     } else{
-        loader.style.display = "block"
-        resultado.style.display = "none"
+        loader.style.display = "block" 
+        resultado.style.display = "none" 
 
-        setTimeout(function() {
+        setTimeout(function() {  //atraso de 1 segundo
             loader.style.display = "none"
             container.style.display = "none"
-            resultado.style.display = "block"
+            resultado.style.display = "block" 
         }, 1000);
 
         var mascaraCalculada = calculaMascara(calculaQntdeEstacao())
@@ -54,6 +60,7 @@ function handleBtnCalcular(){
         var listaEnderecos = primeiroEnd(calculaQntdeEstacao())
         var listaUltimos = ultimoEnd(enderecos(calculaQntdeEstacao()), parseInt(subrede.value))
 
+        //verifica se as listas de endereços são avaliadas
         if (Array.isArray(listaEnderecos) && listaEnderecos.length > 0 && Array.isArray(listaUltimos) && listaUltimos.length > 0) {
             atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantidadeEstacoes, qtdeEnderecos)
         } else {
@@ -62,47 +69,49 @@ function handleBtnCalcular(){
     }
 }
 
+//função para calcular a mascara com base na quantidade de estação
 function calculaMascara(qtdeEstacao) {
-    var mascInput = mascara.value
-
-    var log = Math.log2(qtdeEstacao)
-    var conta = 32 - log
+    var mascInput=mascara.value
+    var log = Math.log2(qtdeEstacao) //calcula logaritmo com base 2 da quantidade de estação
+    var conta = 32 - log //subtrai o log do total de bits 
 
     return conta
 }
 
+//função para calcular  a quantidades de estações
 function calculaQntdeEstacao() {
-    var sub = subrede.value
-    var menos = 32 - mascara.value
-    var qtdeEstacao = (2 ** menos) / sub
+    var sub = subrede.value //obtem o valor da subrede informada pelo usuario
+    var menos = 32 - mascara.value //subtrai o numero de bits com o valor da mascara
+    var qtdeEstacao = (2 ** menos) //obtem a quantidade de estação com 2 elevado ao numero dado na subtração anterior
     return qtdeEstacao
 }
 
-
+//função para calcular o primeiro endereço de cada subrede
 function primeiroEnd(qtdeEstacao) {
     var end = endereco.value
 
-    var ultimoPonto = end.lastIndexOf('.');
-    var ultimoNum = parseInt(end.substring(ultimoPonto + 1))
+    var ultimoPonto = end.lastIndexOf('.'); //pega a posição do último ponto do endereço
+    var ultimoNum = parseInt(end.substring(ultimoPonto + 1)) //pega o último número depois do ponto     
     var qtdeSubredes = parseInt(subrede.value)
 
     const lista = []
     for (var i = 0; i < qtdeSubredes; i++) {
         if (i === 0) {
-            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + 1}`)
+            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + 1}`) //adiciona o primeiro endereço de subrede 
             ultimoNum += 1
         } else {
-            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + qtdeEstacao}`)
+            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + qtdeEstacao}`) //adiciona os demais endereços
             ultimoNum += qtdeEstacao
         }
     }
     return lista
 }
 
+//função para calulcar os enderços de cada subrede
 function enderecos(qtdeEstacao){
     var end = endereco.value
 
-    var ultimoPonto = end.lastIndexOf('.');
+    var ultimoPonto = end.lastIndexOf('.'); 
     var ultimoNum = parseInt(end.substring(ultimoPonto + 1))
     var qtdeSubredes = parseInt(subrede.value)
 
@@ -110,10 +119,10 @@ function enderecos(qtdeEstacao){
     for (var i = 0; i < qtdeSubredes; i++) {
 
         if (i === 0) {
-            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum}`)
+            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum}`)//adiciona o primeiro endereço
         } else {
-            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + qtdeEstacao}`)
-            ultimoNum += qtdeEstacao
+            lista.push(`${end.slice(0, ultimoPonto)}.${ultimoNum + qtdeEstacao}`)//adicona os demais endereços
+            ultimoNum += qtdeEstacao //incrementa o último número pela quantidade de estações
         }
     }
     console.log(lista)
@@ -126,30 +135,31 @@ function enderecos(qtdeEstacao){
 
     const lista2 = []
     for (var i = 0; i < qtdeSubredes2; i++) {
-        lista2.push(`${end2.slice(0, ultimoPonto2)}.${ultimoNum2 + qtdeEstacao - 1}`)
-        ultimoNum2 += qtdeEstacao
+        lista2.push(`${end2.slice(0, ultimoPonto2)}.${ultimoNum2 + qtdeEstacao - 1}`) //adicona o utlimo endereço de cada subrede
+        ultimoNum2 += qtdeEstacao //incrementa o último número pela quantidade de estações
         }
 
-    return [lista, lista2]
+    return [lista, lista2] //retorna as duas listas criada
 }
 
 function ultimoEnd(lista2, qtdeSubredes) {
     const listaUltimos = []
     for (var i = 0; i < qtdeSubredes; i++) {
         var ultimoPonto = lista2[1][i].lastIndexOf('.')
-        var ultimoNum = parseInt(lista2[1][i].substring(ultimoPonto + 1))
+        var ultimoNum = parseInt(lista2[1][i].substring(ultimoPonto + 1)) //pega o ultimo número do endereço
 
-        listaUltimos.push(`${(lista2[1][i]).slice(0, ultimoPonto)}.${ultimoNum - 1}`)
+        listaUltimos.push(`${(lista2[1][i]).slice(0, ultimoPonto)}.${ultimoNum - 1}`) //adicona o utlimo endereço da subrede a lista
         }
 
     return listaUltimos
 }
 
-
+//função para atualizar a inteface
 function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantidadeEstacoes, qtdeEnderecos) {
-    tabela.innerHTML = ''
+    tabela.innerHTML = '' //limpa a tabela
 
-    var trCabecalho = document.createElement('tr')
+    //criando elementnos de uma tabela
+    var trCabecalho = document.createElement('tr') 
     var thSubrede = document.createElement('th')
     var thQtdeEstacao = document.createElement('th')
     var thTodoEndereco = document.createElement('th')
@@ -157,6 +167,7 @@ function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantida
     var thUltimoEndereco = document.createElement('th')
     var thMascara = document.createElement('th')
 
+    //definindo nome das colunas
     thSubrede.textContent = 'Subrede'
     thQtdeEstacao.textContent = 'Qtde de Estações'
     thTodoEndereco.textContent = 'Endereços'
@@ -164,6 +175,7 @@ function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantida
     thUltimoEndereco.textContent = 'Broadcast'
     thMascara.textContent = 'Máscara'
 
+    //adicionando colunas ao cabeçalho
     trCabecalho.appendChild(thSubrede)
     trCabecalho.appendChild(thQtdeEstacao)
     trCabecalho.appendChild(thTodoEndereco)
@@ -171,8 +183,10 @@ function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantida
     trCabecalho.appendChild(thUltimoEndereco)
     trCabecalho.appendChild(thMascara)
 
+    //adicionando o cabeçalho a tabela
     tabela.appendChild(trCabecalho)
 
+    //adicionando as linhas de resultados a tabela
     listaEnderecos.forEach(function(enderecoIP, index) {
         var tr = document.createElement('tr')
         var tdSubrede = document.createElement('td')
@@ -182,14 +196,14 @@ function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantida
         var tdUltimoEndereco = document.createElement('td')
         var tdMascara = document.createElement('td')
 
-        tdSubrede.textContent = (index + 1).toString()
-        tdQtdeEstacao.textContent = quantidadeEstacoes
-        tdTodoEndereco.textContent = `${qtdeEnderecos[0][index]} - ${qtdeEnderecos[1][index]}`
-        tdPrimeiroEndereco.textContent = enderecoIP
-        tdUltimoEndereco.textContent = listaUltimos[index]
-        tdMascara.textContent = `\\${mascaraCalculada}`;
+        tdSubrede.textContent = (index + 1).toString() //define o texto da célula de sub-rede como o índice + 1
+        tdQtdeEstacao.textContent = quantidadeEstacoes //define o texto da célula de quantidade de estações
+        tdTodoEndereco.textContent = `${qtdeEnderecos[0][index]} - ${qtdeEnderecos[1][index]}` //define o texto da célula de endereços
+        tdPrimeiroEndereco.textContent = enderecoIP //define o texto da célula de primeiro endereço
+        tdUltimoEndereco.textContent = listaUltimos[index] //define o texto da célula de broadcast
+        tdMascara.textContent = `\\${mascaraCalculada}` //define o texto da célula de máscara  
 
-
+        //adiciona as cédulas á linha
         tr.appendChild(tdSubrede)
         tr.appendChild(tdQtdeEstacao)
         tr.appendChild(tdTodoEndereco)
@@ -197,6 +211,7 @@ function atualizaTabela(listaEnderecos, listaUltimos, mascaraCalculada, quantida
         tr.appendChild(tdUltimoEndereco)
         tr.appendChild(tdMascara)
 
+        //adiciona a linha de resultados a tabela
         tabela.appendChild(tr)
     })
 }
